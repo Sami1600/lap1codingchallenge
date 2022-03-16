22dfbc8a3a754f89539209a199a12f18d5905e5e
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-03-16 14:36:06 +0000

    changes to API

--- data.js
+++ data.js
@@ -1,64 +1,64 @@
-{
-    "searches"[
-        {
-            "id": 0,
-            "name": "ASOS",
-            "web": "https://www.asos.com/women/",
-            "description": "British online fashion and cosmetic retailer"
-        },
-        {
-            "id": 1,
-            "name": "ZARA",
-            "web": "https://www.zara.com/uk/",
-            "description": "Spanish apparel retailer"
-        },
-        {
-            "id": 2,
-            "name": "Uniqlo",
-            "web": "https://www.uniqlo.com/uk/en/home",
-            "description": "Japanese casual wear retailer, designer and manufactorer"
-        },
-        {
-            "id": 3,
-            "name": "Mango",
-            "web": "https://shop.mango.com/gb/women",
-            "description": "Spanish clothing design and manufactoring company"
-        },
-        {
-            "id": 4,
-            "name": "Nike",
-            "web": "https://www.nike.com/gb/",
-            "description": "American multinational corporation that is engaged in the design, development, manufacturing, and worldwide marketing and sales of footwear, apparel, equipment, accessories, and services."
-        },
-        {
-            "id": 5,
-            "name": "Adidas",
-            "web": "https://www.adidas.co.uk/",
-            "description": "German multinational corporation that designs and manufactures shoes, clothing and accessories"
-        },
-        {
-            "id": 6,
-            "name": "New Balance",
-            "web": "https://www.newbalance.co.uk/",
-            "description": "American sports footwear and apparel brand that was established in 1906"
-        },
-        {
-            "id": 7,
-            "name": "Puma",
-            "web": "https://uk.puma.com/uk/en/home",
-            "description": "German multinational corporation that designs and manufactures athletic and casual footwear, apparel and accessories"
-        },
-        {
-            "id": 8,
-            "name": "Foot Locker",
-            "web": "https://www.footlocker.co.uk/",
-            "description": "American sportswear and footwear retailer"
-        },
-        {
-            "id": 9,
-            "name": "Converses",
-            "web": "https://www.converse.com/uk",
-            "description": "American shoe company that designs, distributes, and licenses sneakers, skating shoes, lifestyle brand footwear, apparel, and accessories"
-        }
-    ]
-}
+// {
+//     const searches = [
+//         {
+//             "id": 0,
+//             "name": "ASOS",
+//             "web": "https://www.asos.com/women/",
+//             "description": "British online fashion and cosmetic retailer"
+//         },
+//         {
+//             "id": 1,
+//             "name": "ZARA",
+//             "web": "https://www.zara.com/uk/",
+//             "description": "Spanish apparel retailer"
+//         },
+//         {
+//             "id": 2,
+//             "name": "Uniqlo",
+//             "web": "https://www.uniqlo.com/uk/en/home",
+//             "description": "Japanese casual wear retailer, designer and manufactorer"
+//         },
+//         {
+//             "id": 3,
+//             "name": "Mango",
+//             "web": "https://shop.mango.com/gb/women",
+//             "description": "Spanish clothing design and manufactoring company"
+//         },
+//         {
+//             "id": 4,
+//             "name": "Nike",
+//             "web": "https://www.nike.com/gb/",
+//             "description": "American multinational corporation that is engaged in the design, development, manufacturing, and worldwide marketing and sales of footwear, apparel, equipment, accessories, and services."
+//         },
+//         {
+//             "id": 5,
+//             "name": "Adidas",
+//             "web": "https://www.adidas.co.uk/",
+//             "description": "German multinational corporation that designs and manufactures shoes, clothing and accessories"
+//         },
+//         {
+//             "id": 6,
+//             "name": "New Balance",
+//             "web": "https://www.newbalance.co.uk/",
+//             "description": "American sports footwear and apparel brand that was established in 1906"
+//         },
+//         {
+//             "id": 7,
+//             "name": "Puma",
+//             "web": "https://uk.puma.com/uk/en/home",
+//             "description": "German multinational corporation that designs and manufactures athletic and casual footwear, apparel and accessories"
+//         },
+//         {
+//             "id": 8,
+//             "name": "Foot Locker",
+//             "web": "https://www.footlocker.co.uk/",
+//             "description": "American sportswear and footwear retailer"
+//         },
+//         {
+//             "id": 9,
+//             "name": "Converses",
+//             "web": "https://www.converse.com/uk",
+//             "description": "American shoe company that designs, distributes, and licenses sneakers, skating shoes, lifestyle brand footwear, apparel, and accessories"
+//         }
+//     ]
+// }
